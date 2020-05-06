4c866ec2cb0b53051ed4b3d8ce26959a82dfa617
Henrik Stanley Mortensen <hemor@dfds.com>
2020-05-06 08:33:52 +0200

    fixed path for update

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 upstreamVersion="$("Command to get replacement")"
 
 set -x
-sed -ri 's/^(ENV NAME_OF_ENV) .*/\1 '"$upstreamVersion"'/' */Dockerfile.builder
+sed -ri 's/^(ENV NAME_OF_ENV) .*/\1 '"$upstreamVersion"'/' */Dockerfile
